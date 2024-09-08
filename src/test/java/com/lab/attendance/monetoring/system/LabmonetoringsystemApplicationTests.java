479ef9bf0e37359ce3e9e9c15b7f75116bddbe5c
Rup2411 <rupesh.baranwal24@gmail.com>
2024-09-08 10:27:27 +0530

    Testing Class

---
+++ src/test/java/com/lab/attendance/monetoring/system/LabmonetoringsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.attendance.monetoring.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabmonetoringsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
